d79e4ab6a1f774eea722322890527d3451f0e2e9
Ernest Joseph Nzallawahe <ernestjoju@gmail.com>
2022-04-06 15:20:31 +0300

    3 api route for derpatment

--- EmployeeTestWebApi/Controllers/DerpatmentController.cs
+++ EmployeeTestWebApi/Controllers/DerpatmentController.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Data.SqlClient;
 
 using Microsoft.Extensions.Configuration;
+using EmployeeTestWebApi.Models;
 
 namespace EmployeeTestWebApi.Controllers
 {
@@ -44,5 +45,84 @@ namespace EmployeeTestWebApi.Controllers
             }
             return new JsonResult(table);
         }
+
+
+        [HttpPost]
+        public JsonResult Post(Derpatment dep )
+        {
+            string query = @"
+                            insert into dbo.Derpatment
+                            values (@DerpatmentName)
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@DerpatmentName", dep.DerpatmentName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Succefuly added");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Derpatment dep)
+        {
+            string query = @"
+                            update dbo.Derpatment
+                            set DerpatmentName= (@DerpatmentName)
+                            where DerpatmentId=@DerpatmentId
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@DerpatmentId", dep.DerpatmentId);
+                    myCommand.Parameters.AddWithValue("@DerpatmentName", dep.DerpatmentName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                            delete from dbo.Derpatment
+                            where DerpatmentId=@DerpatmentId
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@DerpatmentId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
     }
 }
