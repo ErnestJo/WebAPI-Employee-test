2286ec0b84b1f165c900bc86ec22da704c7933b8
Ernest Joseph Nzallawahe <ernestjoju@gmail.com>
2022-04-06 14:53:11 +0300

    Get all departments

---
+++ EmployeeTestWebApi/Models/Derpatment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeTestWebApi.Models
+{
+    public class Derpatment
+    {
+        public int DerpatmentId { get; set; }
+
+        public string DerpatmentName { get; set; }
+
+    }
+}
---
+++ EmployeeTestWebApi/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeTestWebApi.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set;}
+
+        public string EmployeeName { get; set; }
+
+        public string Derpatment { get; set; }
+
+        public DateTime DateOfJoining { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
